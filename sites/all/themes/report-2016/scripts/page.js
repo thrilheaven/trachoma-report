3d871ab4e3b5c609ed362503195ea1f9a2894b31
Nigel Dunn <nigel@redefine.co.uk>
2016-06-11 22:57:10 +0100

    Add cover page text positions and section scrolling

--- sites/all/themes/report-2016/scripts/page.js
+++ sites/all/themes/report-2016/scripts/page.js
@@ -20,3 +20,32 @@
         fixHeader();
     })
 })(jQuery);
+
+/*
+ Scroll to functionality
+ */
+
+(function($) {
+    var slowScroll = function(target, speed) {
+        $('html, body')
+            .stop()
+            .animate(
+                {scrollTop: $(target).offset().top},
+                speed
+            );
+    };
+
+    $(document).ready(function() {
+        $('a[href^="#"]').click(function(event) {
+            var link = $(event.currentTarget),
+                target = link.attr('href'),
+                speed = link.hasClass('main-navigation__item') ? 250 : 1000;
+
+            if (target.length) {
+                event.preventDefault();
+                slowScroll(target, speed);
+            }
+        });
+
+    });
+})(jQuery);
