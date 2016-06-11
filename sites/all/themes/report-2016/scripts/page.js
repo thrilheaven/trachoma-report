623abefca063607204f760c44c4f99143ff4a8d4
Nigel Dunn <nigel@redefine.co.uk>
2016-06-11 17:38:53 +0100

    Add styling for report cover page

---
+++ sites/all/themes/report-2016/scripts/page.js
@@ -0,0 +1,18 @@
+(function($) {
+    var fixHeader = function() {
+        var scrollPoint = $(window).scrollTop(),
+            headerHeight = $('.report__title__heading').height();
+
+        if (scrollPoint > headerHeight) {
+            $('.report__header')
+                .addClass('js-report__header');
+        } else {
+            $('.report__header')
+                .removeClass('js-report__header');
+        }
+    };
+
+    $(document).on('scroll', function() {
+        fixHeader();
+    })
+})(jQuery);
